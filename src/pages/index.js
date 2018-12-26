d7d7f63f01559bf1a2f08b830ffa5a5121deefb7
jacobweber <jacobaweber@gmail.com>
2018-12-26 11:09:21 -0600

    inital commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './index.scss'
+
+const homePage = () => (
+  <div className="homePage">
+    <div className="inner">
+      <img alt="Knight Cap" src={require('../images/Logo.png')} />
+      <p>Something new is in the works, stay tuned for more.</p>
+    </div>
+  </div>
+)
+
+export default homePage;
---
+++ src/utils/typography.js
@@ -0,0 +1,16 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.45,
+  headerFontFamily: [
+    "Avenir Next",
+    "Helvetica Neue",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Georgia", "serif"],
+})
+
+export default typography
