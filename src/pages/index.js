9df7a1f8eda4b6edab6079742fbb977bf300afeb
jacobweber <jacobaweber@gmail.com>
2018-12-27 23:01:01 -0600

    Adding Drupal Gatsby module.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import './index.scss'
 const homePage = () => (
   <div className="homePage">
     <div className="inner">
-      <img alt="Knight Cap" src={require('../images/Logo.png')} />
-      <p>Testing another somethings new is in the works, stay tuned for more.</p>
+      <img alt="She Bakes He Cooks" src={require('../images/Logo.png')} />
+      <p>Something new is in the works, stay tuned for more.</p>
     </div>
   </div>
 )
