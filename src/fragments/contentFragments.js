b5c89997720f7aee70acc233a584b6d8006e2af9
jacobweber <jacobaweber@gmail.com>
2019-06-19 17:01:14 -0500

    starting setup to pull paragraph component content from Drupal

---
+++ src/fragments/contentFragments.js
@@ -0,0 +1,55 @@
+import { graphql } from "gatsby"
+
+export const nodeRecipeFragment = graphql`
+  fragment nodePageFragment on node__page {
+    nid: drupal_internal__nid
+    title
+    subtitle: field_short_title
+    summary: field_summary
+    path {
+      alias
+    }
+    r: relationships {
+      content: field_content {
+        __typename
+        ... on paragraph__breaker {
+          ...breakerFragment
+        }
+        ... on paragraph__card {
+          ...cardFragment
+        }
+        ... on paragraph__card_list {
+          ...cardListFragment
+        }
+        ... on paragraph__faq {
+          ...faqFragment
+        }
+        ... on paragraph__gallery_carousel {
+          ...galleryCarouselFragment
+        }
+        ... on paragraph__hero_media {
+          ...heroMediaFragment
+        }
+        ... on paragraph__map {
+          ...mapFragment
+        }
+        ... on paragraph__quote {
+          ...quoteFragment
+        }
+      }
+      thumbnail: field_thumbnail {
+        r: relationships {
+          image: field_image {
+            file: localFile {
+              cis: childImageSharp {
+                fluid(maxHeight: 200) {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
